e645cdd1c5b457db28780d135543e64a78b0c1a9
Jojocode12 <joseph.combourieu@gmail.com>
2018-12-02 19:37:10 +0100

    Get PlayerController from tank AI controller

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -2,6 +2,20 @@
 
 #include "TankPlayerController.h"
 
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto ControlledTank = GetControlledTank();
+
+	if (!ControlledTank) {
+		UE_LOG(LogTemp, Warning, TEXT("Couldn't find possessing tank!"));
+	}
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("Found possessing tank: %s"), *ControlledTank->GetName());
+	}
+}
+
 ATank* ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
